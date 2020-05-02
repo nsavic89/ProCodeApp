0f6ff2cff87e6db89325ab5c03af19abc77c057a
Nenad Savic <nenadschem@gmail.com>
2020-05-02 19:43:13 +0200

    database name changed to default in settings.py

--- procodeRestApi/procodeRestApi/settings.py
+++ procodeRestApi/procodeRestApi/settings.py
@@ -114,7 +114,7 @@ WSGI_APPLICATION = 'procodeRestApi.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'mydatabase',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
     }
 }
 
