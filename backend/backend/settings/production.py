30658970e771c604876e786e9a0aa5f58707ec7c
Nenad Savic <nenadschem@gmail.com>
2020-05-31 23:40:07 +0200

    Settings MODULE for production updated regarding mysql db

--- backend/backend/settings/production.py
+++ backend/backend/settings/production.py
@@ -8,3 +8,16 @@ ALLOWED_HOSTS = ['api.pro-code.ch']
 CORS_ORIGIN_WHITELIST = [
     "http://www.pro-code.ch"
 ]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+       	'NAME': 'procode_db',
+	'USER': 'nenad',
+	'PASSWORD': 'Procode-2020',
+	'HOST': '',
+	'PORT': ''
+    }
+}
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
